3416135fd3b6ba630310468c02c129dc2215a052
Sajed Nahian <snahian@stuy.edu>
2019-03-03 14:22:58 -0500

    removed print statement

--- display.py
+++ display.py
@@ -46,8 +46,6 @@ def save_ppm( screen, fname ):
 
 def save_extension( screen, fname ):
     ppm_name = fname[:fname.find('.')] + '.ppm'
-    print(ppm_name)
-    print(fname)
     save_ppm( screen, ppm_name )
     p = Popen( ['convert', ppm_name, fname ], stdin=PIPE, stdout = PIPE )
     p.communicate()
